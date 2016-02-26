53386de8bafa1f38579334ac083d4fd4cda57009
valeriestauffer <valerie.stauffer@heig-vd.ch>
2016-02-26 15:32:57 +0100

    Modif IssueType controller

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+async = require('async'),
 router = express.Router(),
 mongoose = require('mongoose'),
 User =mongoose.model('User');
@@ -23,13 +24,11 @@ function findUser(req, res, next){
 	});
 }
 
-//POST /api/v1/users 
-//Add a user and his role
 
 /**
  * @api {post} /users Add a user
- * @apiName GetUser
- * @apiGroup User
+ * @apiName addUser
+ * @apiGroup Users
  *
  * @apiParam {Number} id Users unique ID.
  *
@@ -94,15 +93,6 @@ router.post('/', function (req, res, next){
  *       "error": "UserNotFound"
  *     }
  */
-router.get('/', function(req, res, next){
-	User.find(function (err,user){
-		if (err){
-			res.status(500).send(err);
-			return;
-		}
-	res.send(user);
-	});
-});
 
 //Get /api/v1/users selon ID
 //get a user
@@ -230,19 +220,6 @@ router.delete('/:idUser/role/:role',findUser,function(req, res, next){
 });
 
 
-//Get /api/v1/users
-//get the list of all users "citizen"
-router.get('/api/v1/', function(req, res, next){
-	User.find(role, function (err,user){
-		if (err){
-			res.status(500).send(err);
-			return;
-		}
-	res.send(user);
-	});
-});
-
-
 
 //Get /api/v1/users selon ID
 //get the list of the issues raised by a user
@@ -250,6 +227,7 @@ router.get('/api/v1/', function(req, res, next){
 router.get('/:idUser/issue', findUser, function(req, res, next){
 	var idUser = req.params.idUser;
 	var criteria = {"author":idUser};
+
 
 	Issue.find(criteria, function(err, issue){
 		if(err){
@@ -261,3 +239,132 @@ router.get('/:idUser/issue', findUser, function(req, res, next){
 });
 
 
+
+
+//Get /api/issues
+router.get('/', function(req, res, next){
+	var criteria = {};
+
+	console.log("bou");
+
+	if (req.query.limit) {
+		var limitUser = req.query.limit;
+	}
+
+	if (req.query.status) {
+		criteria.status = req.query.status;
+	}
+
+	if (req.query.role) {
+		criteria.role ={$in:[req.query.role]};
+	}
+
+	// Get page and page size for pagination.
+	var page = req.query.page ? parseInt(req.query.page, 10) : 1,
+	pageSize = req.query.pageSize ? parseInt(req.query.pageSize, 10) : 30;
+
+	// Convert page and page size to offset and limit.
+	var offset = (page - 1) * pageSize,
+	limit = pageSize;
+
+	// Count all users (without filters).
+	function countAllUsers(callback) {
+		User.count(function(err, totalCount) {
+	  		if (err) {
+	    		callback(err);
+	  		} else {
+	    		callback(undefined, totalCount);
+	  		}
+		});
+	}
+
+
+
+  	// Count books matching the filters.
+  	function countFilteredUsers(callback) {
+    	User.count(criteria, function(err, filteredCount) {
+      	if (err) {
+        		callback(err);
+      		} else {
+        		callback(undefined, filteredCount);
+      		}
+    	});
+  	}
+
+  	// Find books matching the filters.
+  	function findMatchingUsers(callback) {
+
+    	var query = User
+      	.find(criteria)
+
+      	// Do not forget to sort, as pagination makes more sense with sorting.
+      	.sort('name.first')
+      	.skip(offset)
+      	.limit(limit);
+
+    	// Embed publisher object if specified in the query.
+    	if (req.query.embed == 'publisher') {
+      		query = query.populate('publisher');
+    	}
+
+    	// Execute the query.
+    	query.exec(function(err, users) {
+      		if (err) {
+        		callback(err);
+      		} else {
+        		callback(undefined, users);
+      		}
+    	});
+  	}
+
+  	function findUsersByIssuesStatus(callback){
+  		var issues;
+
+  		Issue.find(function(err, issuesTmp) {
+	  		if (err) {
+	    		callback(err);
+	  		} else {
+	    		issues=issuesTmp;
+	  		}
+		});
+
+    	// Embed publisher object if specified in the query.
+    	if (req.query.embed == 'publisher') {
+      		query = query.populate('publisher');
+    	}
+
+    	// Execute the query.
+    	query.exec(function(err, users) {
+      		if (err) {
+        		callback(err);
+      		} else {
+        		callback(undefined, users);
+      		}
+    	});
+  	}
+  	// Set the pagination headers and send the matching books in the body.
+  	function sendResponse(err, results) {
+    	if (err) {
+      		res.status(500).send(err);
+      		return;
+    	}
+
+    	var totalCount = results[0],
+        filteredCount = results[1],
+        users = results[2];
+
+    	// Return the pagination data in headers.
+    	res.set('X-Pagination-Page', page);
+    	res.set('X-Pagination-Page-Size', pageSize);
+    	res.set('X-Pagination-Total', totalCount);
+    	res.set('X-Pagination-Filtered-Total', filteredCount);
+
+    	res.send(users);
+  	}
+
+	async.parallel([
+    countAllUsers,
+    countFilteredUsers,
+    findMatchingUsers
+  	], sendResponse);
+});
